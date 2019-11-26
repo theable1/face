e773b1b029824736bcaac2c854c4d69da1d8c8d7
dell <1229616755@qq.com>
2019-11-26 15:04:24 +0800

    Merge branch 'master' of https://github.com/theable1/face
    # Conflicts:
    #	src/main/resources/templates/search.html

--- src/test/java/com/ffcs/face/FaceApplicationTests.java
+++ src/test/java/com/ffcs/face/FaceApplicationTests.java
@@ -1,7 +1,12 @@
 package com.ffcs.face;
 
+import com.ffcs.face.util.ImageUtils;
+import net.minidev.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
 class FaceApplicationTests {
@@ -11,11 +16,26 @@ class FaceApplicationTests {
     }
 
     @Test
-    void testHash(){
-        String msg = "23821379210";
-        for (int i = 0; i < 5; i++) {
-            int hashCode = msg.hashCode();
-            System.out.println(hashCode);
-        }
+    void testHash() {
+        //访问路径
+        String url="http://localhost:10004/bvs/api/v1/faceinfo/feature";
+        //设置请求头,可根据接口文档要求设置
+        HttpHeaders header = new HttpHeaders();
+        header.set("Accept-Charset", "UTF-8");
+        header.set("Content-Type", "application/json; charset=utf-8");
+        //设置请求参数，此处设置为json
+        JSONObject object = new JSONObject();
+        object.put("image_id","1");
+        object.put("image_b64","/9j/4Q/cRXhpZgAATU");
+        //entity为请求体，包含请求参数
+
+        HttpEntity entity = new HttpEntity(object.toJSONString(), header);
+        System.out.println(entity);
+        RestTemplate restTemplate = new RestTemplate();
+        //发送post请求
+        String res = restTemplate.postForObject(url, entity, String.class);
+        System.out.println(res);
+
     }
+
 }
